7a79d2b60ace31061a821aef00cd5c34db2a8dff
PogiAbel <pogonyiabel@gmail.com>
2023-04-12 12:58:31 +0200

    search in list and call function for result

--- gyak5/struct_with_thread_safety/main.c
+++ gyak5/struct_with_thread_safety/main.c
@@ -2,15 +2,46 @@
 #include <stdlib.h>
 #include "mystruct.h"
 
+int search_list(LinkedList *ll, int value) {
+    pthread_mutex_lock(&ll->lock);
+    Node *current = ll->head;
+    while (current != NULL) {
+        if (current->data == value) {
+            pthread_mutex_unlock(&ll->lock);
+            return 1;
+        }
+        current = current->next;
+    }
+    pthread_mutex_unlock(&ll->lock);
+    return 0;
+}
+
+void found_inlist(){
+    printf("Found in list");
+}
+
+void not_found_inlist(){
+    printf("Not found in list");
+}
+
+void search_func(LinkedList *ll, int value, void (*func)(), void (*func2)()){
+    if(search_list(ll, value)){
+        func();
+    }else{
+        func2();
+    }
+}
+
 int main(int argc, char const *argv[])
 {
-    Stack s;
-    initialize_stack(&s);
-    push(&s, 50);
-    push(&s, 22);
-    int last = pop(&s);
-    printf("%d", last);
-    last = pop(&s);
-    printf("%d", last);
+    LinkedList ll;
+    initialize_linked_list(&ll);
+    insert_list(&ll, 10);
+    insert_list(&ll, 20);
+    insert_list(&ll, 30);
+    insert_list(&ll, 40);
+
+    search_func(&ll, 23, found_inlist, not_found_inlist);
+
     return 0;
 }
