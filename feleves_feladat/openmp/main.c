a2829b8c2886c4258bbca8b6f168bdf30fb99953
PogiAbel <pogonyiabel@gmail.com>
2023-04-26 13:39:53 +0200

    add parallel method and reshape the struct

--- feleves_feladat/openmp/main.c
+++ feleves_feladat/openmp/main.c
@@ -9,6 +9,12 @@ struct Point
     double y;
 } typedef Point;
 
+struct Result
+{
+    double pi;
+    double time;
+} typedef Result;
+
 Point random_point(){
     // gives random point between 0 and 1
     Point p;
@@ -25,7 +31,6 @@ int is_in_circle(Point p){
 
 double linear_pi(int n){
     // calculates pi with linear method
-    srand(time(NULL));
     int in_circle = 0;
     int out_circle = 0;
     for (int i = 0; i < n; i++)
@@ -40,16 +45,60 @@ double linear_pi(int n){
     return (double)in_circle / (double)n * 4;
 }
 
+double parallel_pi(int n, int thread_count){
+    // calculates pi with parallel method
+    int in_circle = 0;
+    omp_lock_t lock;
+    omp_init_lock(&lock);
+    int group_size = n / thread_count;
+
+    #pragma omp parallel for
+    for (int i = 0; i < thread_count; i++)
+    {
+        int local_count = 0;
+        for (int i = 0; i < group_size; i++)
+        {
+            Point p = random_point();
+            if(is_in_circle(p)){
+                local_count++;
+            }
+        }
+        omp_set_lock(&lock);
+        in_circle += local_count;
+        omp_unset_lock(&lock);
+
+    }
+    return (double)in_circle / (double)n * 4;
+}
+
+Result linear(int n){
+    // calculate pi with linear
+    clock_t start, end;
+    Result result;
+
+    start = clock();
+    double liner_pi = linear_pi(n);
+    end = clock();
+    return (Result){liner_pi, (double)(end - start) / CLOCKS_PER_SEC};
+}
+
+Result parallel(int n, int thread_count){
+    // calculate pi with parallel
+    clock_t start, end;
+    Result result;
+
+    start = clock();
+    double pi_par = parallel_pi(n, thread_count);
+    end = clock();
+    return (Result){pi_par, (double)(end - start) / CLOCKS_PER_SEC};
+}
+
 int main(int argc, char const *argv[])
 {
+    // init values
     srand(time(NULL));
     clock_t start, end;
     int n = 500000;
-    start = clock();
-    double pi = linear_pi(n);
-    end = clock();
-    printf("pi: %f\n", pi);
-    printf("time: %f\n", (double)(end - start) / CLOCKS_PER_SEC);
-    
+
     return 0;
 }
