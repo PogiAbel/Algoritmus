baedb68e1aa87179b41a0a50aa41e0476d76d31c
PogiAbel <pogonyiabel@gmail.com>
2023-05-02 14:57:05 +0200

    made 3d statics

--- feleves_feladat/openmp/main.c
+++ feleves_feladat/openmp/main.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <omp.h>
 #include <time.h>
+#include <math.h>
 
-#define PI 3.141592
+#define PI 3.14159265358979323846
 
 struct Point
 {
@@ -159,33 +160,28 @@ Result parallel_3d(int n, int thread_count){
     return (Result){pi_par, (double)(end - start) / CLOCKS_PER_SEC};
 }
 
-int main(int argc, char const *argv[])
-{
-    // init values
-    srand(time(NULL));
-    clock_t start, end;
-
-    double result_matrix[8][6];
+void write_result_to_file(){
+    double result_matrix[22][6];
     result_matrix[0][0] = 0;
     int row, col = 1;
 
     FILE *fp;
-    fp = fopen("result_2d.txt", "w");
+    fp = fopen("./statics/result_3d_2.txt", "w");
 
     for (int n = 100000000; n <= 500000000; n+=100000000)
     {
         row = 1;
 
-        Result linear_result = linear(n);
+        Result linear_result = linear_3d(n);
 
         result_matrix[0][col] = n;
         result_matrix[row][0] = 1;
         result_matrix[row][col] = linear_result.time;
 
-        for (int thread_count = 20; thread_count <= 120; thread_count+=20)
+        for (int thread_count = 2; thread_count <= 20; thread_count+=2)
         {
             row ++; 
-            Result parallel_result = parallel(n, thread_count);
+            Result parallel_result = parallel_3d(n, thread_count);
             result_matrix[row][col] = parallel_result.time;
             result_matrix[row][0] = thread_count;
         }
@@ -205,7 +201,53 @@ int main(int argc, char const *argv[])
 
     fclose(fp);
     printf("Done\n");
+}
 
+void pi_estimation_limit(){
+    double pi_estimation = 0;
+    double limit = 0.000001;
+    int in_circle = 0;
+    int all_count = 0;
+    omp_lock_t lock;
+    omp_init_lock(&lock);
+
+    while (fabs(pi_estimation-PI) > limit)
+    {
+
+        #pragma omp parallel for
+        for (int i = 0; i < 10; i++)
+        {
+            int local_count = 0;
+            for (int j = 0; j < 500; j++)
+            {
+                Point p = random_point();
+                if(is_in_circle(p)){
+                    local_count++;
+                }
+            }
+            omp_set_lock(&lock);
+            all_count += 500;
+            in_circle += local_count;
+            omp_unset_lock(&lock);
+
+        }
+
+        pi_estimation =  (double)in_circle / (double)all_count * 4;
+    }
+    printf("Estimation limit: %f\n", limit);    
+    printf("Total count: %d\n", all_count);    
+    
+}
+
+int main(int argc, char const *argv[])
+{
+    srand(time(NULL));
+    // clock_t start, end;
+    // start = clock();
+    // pi_estimation_limit();
+    // end = clock();
+    // printf("Time: %f\n", (double)(end - start) / CLOCKS_PER_SEC);
+    write_result_to_file();
 
     return 0;
 }
--- feleves_feladat/openmp/readme.md
+++ feleves_feladat/openmp/readme.md
@@ -40,11 +40,17 @@ területének közelítéséhez a következő képletet használjuk:
 - Jól látszik hogy mennyivel jobb eredményeket kapott a párhuzamosítás, azonban több szálon már nem tudott tovább javulni a program, sőt a 120 szálon futtatott program volt, hogy lassabb lett mint a 100 szálon futtatott program. Ez azért lehet mert a szálak közötti kommunikáció már több időt vesz igénybe mint amennyit a szálak párhuzamosításával nyertünk.
 ### - A programot 20-120 szálon futtattam 20-as lépésekben, 50000000 pont generálásával.
 ![eredmény](./picture/result_2d_1.png)
+### - A programot 2-20 szálon futtattam 2-es lépésekben, 500000000 pont generálásával.
+![eredmény](./picture/result_2d_3.png)
+### - A program addig fut amíg el nem éri a delta értéket, a π és a közelítés közötti különbség nem nagyobb mint a delta.
+- A futási rétékek különböztek mindig, mivel random szám generátor van benne, ezért mindig más eredmény jön ki, de a különbség nem nagy.
+- Legjobb elért eredményem: delta: 0.000001, eléréséhez szükséges pontok száma: 3175000, elérési idő: 0.139000 s
 
-## Algoritmus használata 3 dimenzióban:
+# Algoritmus használata 3 dimenzióban:
 - A kör helyett a gömböt vesszük alapul, a gömb sugara 1, így a Pitagorasz-tétel a<sup>2</sup> + b<sup>2</sup> + c<sup>2</sup> <= 1<sup>2</sup> = d<sup>2</sup> = 1
-- A gömb területének közelítéséhez a következő képletet használjuk :
-π = 6 * (gömbbe eső pontok száma) / (összes pont száma)
-- A gömb területe: 4 * π * r<sup>2</sup> = 4 * π * 1<sup>2</sup> = 4 * π
-- A gömb területének közelítése: 4 * π = 6 * (gömbbe eső pontok száma) / (összes pont száma)
+- A gömb térfogatának egyenlete: V = 4/3 * π * r<sup>3</sup>
+- A Monte Carlo közelítéssel V<sub>est</sub> = V<sub>cube</sub> * (gömbbe eső pontok száma) / (összes pont száma)
 - π közelítése: π = 6 * (gömbbe eső pontok száma) / (összes pont száma)
+- Amely a gömb felületének és a kocka felületének arányából adódik
+![eredmény](./picture/result_3d.png)
+![eredmény](./picture/result_3d_2.png)
