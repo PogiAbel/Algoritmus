9b1af2a5de5562e4787d0a6f571486a33ae3d20e
PogiAbel <pogonyiabel@gmail.com>
2023-05-09 10:56:48 +0200

    remake parallel function

--- feleves_feladat/pthread/parallel.c
+++ feleves_feladat/pthread/parallel.c
@@ -6,13 +6,13 @@
 #include <string.h>
 #include <time.h>
 
-typedef struct {
-    double *matrix;
-    double *identity_matrix;
-    int size;
-    int thread_num;
-    int num_threads;
-} invert_args;
+typedef struct thread_data{
+    double *A;
+    int n;
+    int start_row;
+    int end_row;
+    int j;
+} thread_data;
 
 int random_number(){
     // return random intiger between 0 and 99
@@ -38,23 +38,6 @@ double* random_matrix(int n){
     for (int i = 0; i < n*n; i++)
     {
         matrix[i] = (double)random_number();
-    }
-    return matrix;
-}
-
-double* indentity_matrix(int n){
-    // return indentity matrix with n*n size
-    double* matrix = (double*)malloc(n*n*sizeof(double));
-    for (int i = 0; i < n*n; i++)
-    {
-        if (i % (n+1) == 0)
-        {
-            matrix[i] = 1;
-        }
-        else
-        {
-            matrix[i] = 0;
-        }
     }
     return matrix;
 }
@@ -102,79 +85,73 @@ int is_invertible(double *matrix, int n) {
     return det != 0;  // matrix is invertible if determinant is non-zero
 }
 
-void *invert_worker(void *arg) {
-    invert_args *args = (invert_args *)arg;
-    double *matrix = args->matrix;
-    double *identity_matrix = args->identity_matrix;
-    int size = args->size;
-    int thread_num = args->thread_num;
-    int num_threads = args->num_threads;
-
-    int i, j, k;
-    for (k = 0; k < size; k++) {
-        if (k % num_threads != thread_num) {
-            continue;
-    }
-    int i, j, k;
-    double pivot, temp;
-    for (k = 0; k < size; k++) {
-            if (k % num_threads != thread_num) {
-            continue;
-            }
-            pivot = matrix[k*size + k];
-            if (fabs(pivot) < 0.01) {
-                // pivot is too small, matrix is singular
-                return;
-            }
-            for (j = 0; j < size; j++) {
-                matrix[k*size + j] /= pivot;
-                identity_matrix[k*size + j] /= pivot;
-            }
-            for (i = 0; i < size; i++) {
-                if (i == k) {
-                    continue;
-                }
-                temp = matrix[i*size + k];
-                for (j = 0; j < size; j++) {
-                    matrix[i*size + j] -= temp * matrix[k*size + j];
-                    identity_matrix[i*size + j] -= temp * identity_matrix[k*size + j];
-                }
-            }
-
+void *subtract_rows(void *arg) {
+    struct thread_data *data = (struct thread_data *) arg;
+    double *A = data->A;
+    int n = data->n;
+    int start_row = data->start_row;
+    int end_row = data->end_row;
+    int j = data->j;
+    int i, k;
+    double factor;
+    for (i = start_row; i < end_row; i++) {
+        factor = A[i*n+j] / A[j*n+j];
+        for (k = j; k < n; k++) {
+            A[i*n+k] -= factor * A[j*n+k];
         }
-
     }
     pthread_exit(NULL);
 }
 
-void matrix_inverse_parallel(double *matrix, double *identity_matrix, int size, int num_threads) {
-    pthread_t *threads = (pthread_t *)malloc(num_threads * sizeof(pthread_t));
-    invert_args *args = (invert_args *)malloc(num_threads * sizeof(invert_args));
-    int i;
-    for (i = 0; i < num_threads; i++) {
-        args[i].matrix = matrix;
-        args[i].identity_matrix = identity_matrix;
-        args[i].size = size;
-        args[i].thread_num = i;
-        args[i].num_threads = num_threads;
-        pthread_create(&threads[i], NULL, invert_worker, &args[i]);
+void inplace_inverse(double* A, int n, int num_threads){
+    int i,j,k,pivot;
+    double temp;
+    int rows_per_thread = (n-1) / num_threads + 1;
+
+    for (j = 0; j < n; j++) {
+        // Find pivot row
+        pivot = j;
+        for (i = j+1; i < n; i++) {
+            if (fabs(A[i*n+j]) > fabs(A[pivot*n+j])) {
+                pivot = i;
+            }
+        }
+        if (pivot != j) {
+            // Swap rows
+            for (k = 0; k < n; k++) {
+                temp = A[j*n+k];
+                A[j*n+k] = A[pivot*n+k];
+                A[pivot*n+k] = temp;
+            }
+        }
+        // Subtract scaled pivot row from other rows
+        pthread_t threads[num_threads];
+        struct thread_data thread_data_array[num_threads];
+        int thread_index = 0;
+        for (i = j+1; i < n; i += rows_per_thread) {
+            struct thread_data *data = &thread_data_array[thread_index];
+            data->A = A;
+            data->n = n;
+            data->start_row = i;
+            data->end_row = i + rows_per_thread;
+            if (data->end_row > n) {
+                data->end_row = n;
+            }
+            data->j = j;
+            pthread_create(&threads[thread_index], NULL, subtract_rows, (void *) data);
+            thread_index++;
+        }
+        for (i = 0; i < num_threads; i++) {
+            pthread_join(threads[i], NULL);
+        }
     }
-    for (i = 0; i < num_threads; i++) {
-        pthread_join(threads[i], NULL);
-    }
-    free(threads);
-    free(args);
 }
 
-int main(int argc, char const *argv[])
-{
-    srand(time(NULL));
+float main_function(int n, FILE* fp){
     clock_t start, end;
-    int n = argv[1] ? atoi(argv[1]) : 3;
-    printf("n: %d\n", n);
 
+    // create invertible matrix
     double* matrix = random_matrix(n);
-    double* identity_matrix = indentity_matrix(n);
 
     int invertible = is_invertible(matrix, n);
 
@@ -184,13 +161,31 @@ int main(int argc, char const *argv[])
         matrix = random_matrix(n);
         invertible = is_invertible(matrix, n);
     }
-    printf("The matrix is invertible\n");
     
+    // inverse matrix
+    for (int  i = 5; i <= 20; i+=5)
+    {
+        start = clock();
+        inplace_inverse(matrix, n, i);
+        end = clock();
+        fprintf(fp, "%d, %d, %f\n", n, i, (float)(end - start) / CLOCKS_PER_SEC);
+    }
     
-    start = clock();
-    matrix_inverse_parallel(matrix, identity_matrix, n, 2);
-    end = clock();
 
-    printf("Time: %f\n", (double)(end - start) / CLOCKS_PER_SEC);
+    return (float)(end - start) / CLOCKS_PER_SEC;
+}
+
+int main(int argc, char const *argv[])
+{
+    srand(time(NULL));
+    FILE* fp = fopen("statics/data_parallel.txt", "w");
+
+    for (int i = 200; i <= 1400; i+=200)
+    {
+        main_function(i, fp);
+    }
+
+    fclose(fp);
+   
     return 0;
 }
